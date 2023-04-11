2e28603e6485f72559675aecb624e4d21f58ca4b
timsonau <117775073+timsonau@users.noreply.github.com>
2023-04-10 19:08:20 -0500

    Fuzzing

--- Workshop/Workshop-8/workshop8.py
+++ Workshop/Workshop-8/workshop8.py
@@ -1,48 +1,75 @@
 '''
-Author: Akond Rahman
+Author: Tim Son
 Code needed for Workshop 8
 '''
 
+import traceback
 from ast import operator
-import random
-
+import random 
 
 def divide(v1, v2):
-    temp = 0
-    if (isinstance(v1, int) and isinstance(v2, int)):
-        if v2 > 0:
-            temp = v1/v2
-        elif v2 < 0:
-            temp = v1/v2
+    temp = 0 
+    if (isinstance(v1, int))  and (isinstance(v2, int)): 
+       if v2 >  0:
+          temp =  v1 / v2
+       elif v2 < 0:
+          temp = v1 / v2 
+       else:
+          print("Divisor is zero. Provide non-zero values.")
+    else: 
+       temp = "Invalid input. Please provide numeric values."    
+    return temp 
+
+def simpleCalculator(v1, v2, op):
+    try:
+        if (op == '/'):
+            return v1 / v2
+        elif (op == '*'):
+            return v1 * v2  
+        elif(op == "-"):
+            return v1 - v2
+        elif(op == "+"):
+            return v1+v2
         else:
-            temp = "Divisior is zero. provide non-zero values."
-    else:
-        temp = 'please provide numeric values'
-    return temp
-
-
+            print('Invalid Operand')
+            return 0
+    except Exception as e:
+        print("\n*****ERROR*****")
+        traceback.print_exc()
+    
 def fuzzValues():
-    # positive or expected software testing
-    res = divide(2, 1)
-    print(res)
-    print('='*100)
-    # negative software testing: > 0 divisior test
-    res = divide(2, 0)
-    print(res)
-    print('='*100)
-    # negative software testing: > 0 divisior test
-    res = divide(2, -1)
-    print(res)
-    print('='*100)
-    # negative software testing:  non string divisior
-    res = divide(2, '1')
-    print(res)
+    #In-Class
+    # positive or expected software testing 
+    #res = divide(2, 1)
+    # negative software testing: > 0 divisor test 
+    #res = divide(2, 0)
+    # negative software testing: <0 divisor test 
+    #res = divide(2, -1)
+    # negative software testing: check types: example 1  
+    res = divide(2, '1')  
+    # negative software testing: check types: example 2 
+    res = divide('2', '1') 
+    print(res)   
     print('='*100)
 
+    #Workshop 8
+    #error 1 v1=String and v2=int divider
+    simpleCalculator('8', 2, '/')
+    #error 2 Zero divider
+    simpleCalculator(3, 0, '/')
+    #error 3 v1=String sequence and v2=non-int multiply
+    simpleCalculator('🏳0🌈️', ' ॣh ॣ ॣ冗', '*')
+    #error 4  v1=float and v2=string divider
+    print(simpleCalculator(-777E+02332434422132, '-555333E+102', '/'))
+    #error 5 v1=nonetype and v2=int addition
+    simpleCalculator(None, 7, '+')
+    #error 6 v1=list and v2=list multipliy
+    simpleCalculator([8], [8], '*')
+    #error 7 v1=int and v2=set multiply 
+    simpleCalculator(0xA0F, {22}, '*')
 
-def simpleFuzzer():
+def simpleFuzzer(): 
     fuzzValues()
 
-
-if __name__ == '__main__':
+if __name__=='__main__':
     simpleFuzzer()
